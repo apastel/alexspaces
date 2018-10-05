21081c6d045097208ae8e2eee445ca1c77a24abe
Alex Pastel <alex.r.pastel@gmail.com>
2018-10-05 00:31:15 -0700

    Adding snake

---
+++ snake/src/main.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+from lib.game import Game
+import atexit
+import config
+from sense_hat import SenseHat
+
+
+def main(sense):
+    game = Game(
+        config.game['columns'],
+        config.game['rows'],
+        config.game['fps'],
+        config.game['countdown'],
+        config.game['interval'],
+        config.game['score_increment'],
+        config.game['level_increment'],
+        config.game['interval_increment'],
+        sense
+    )
+
+    game.start()
+
+    atexit.register(game.__exit__)
+
+
+if __name__ == '__main__':
+    sense = SenseHat()
+    sense.set_rotation(180)
+    main(sense)
