6d8827eb7e3a19eee629761bd4b9f5474b9fd64e
Alex Pastel <alex.pastel@moesol.com>
2019-09-27 16:43:14 -0700

    Install all dependencies for simulator

--- run_me.py
+++ run_me.py
@@ -10,24 +10,26 @@ import sys
 import subprocess
 
 def _is_package_installed(package_name):
+    print(f'Checking if {package_name} is installed...', end='')
     result = subprocess.run(['dpkg', '-s', package_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if result.returncode == 0:
+        print('YES')
         return True
     stdout = result.stdout.decode('utf-8')
     stderr = result.stderr.decode('utf-8')
     if 'not installed' in stderr:
+        print('NO')
         return False
     else:
         print(stdout)
         print(stderr, file=sys.stderr)
-        raise('Failed to verify %s was installed' % package_name)
+        raise(f'Failed to verify {package_name} was installed')
 
-if not _is_package_installed('python3-venv'):
-    print('Attempting to install python3-venv...')
-    subprocess.run(['sudo', 'apt', '-y', 'install', 'python3-venv'], check=True)
-    print('Attempting to install python3-venv...DONE.')
-
-subprocess.run(['sudo', 'apt', '-y', 'install', 'libgirepository1.0-dev', 'gcc', 'libcairo2-dev', 'pkg-config', 'python3-dev', 'gir1.2-gtk-3.0'], check=True)
+for package in [line.rstrip('\n') for line in open('apt-requirements.txt')]:
+    if not _is_package_installed(package):
+        print(f'Attempting to install {package}...')
+        subprocess.run(['sudo', 'apt', '-y', 'install', package], check=True)
+        print(f'Attempting to install {package}...DONE.')
 
 print('Initializing python3-venv...')
 # Creates a Virtual Environment containing Python3 and Pip3
